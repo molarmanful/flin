646ec19ab2ca2aa95cd7497915c294add08657f3
Benjamin Pang <benjinpang@gmail.com>
2022-09-04 18:13:32 -0400

    add ci

--- dev.sh
+++ dev.sh
@@ -1,1 +1,6 @@
-dotnet pack LIN.Cli/LIN.Cli.fsproj && dotnet tool update -g --add-source nupkg LIN.Cli
+#!/usr/bin/env bash
+
+set -e
+
+dotnet pack LIN.Cli/LIN.Cli.fsproj
+dotnet tool update -g --add-source nupkg LIN.Cli
